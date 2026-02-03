37e8d092b681fb9fe7a82bf5b36b8c6c326d65d1
Vishal Ambhore <vishalambhore2954@gmail.com>
2026-02-03 19:33:56 +0530

    not found page with wild card route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Register } from './Component/UnAuth/register/register';
 import { Login } from './Component/UnAuth/login/login';
+import { Notfound } from './Component/Auth/notfound/notfound';
 
 export const routes: Routes = [
     { path: '', component:Login},
     { path: 'login', component:Login},
-    { path: 'register',component:Register}
+    { path: 'register', component:Register},
+    { path: '**', component:Notfound}
 ];
