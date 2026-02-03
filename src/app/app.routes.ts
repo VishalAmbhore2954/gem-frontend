f4a67e37aca5cc0f41e0dcbf5a14c4e642c5409d
Vishal Ambhore <vishalambhore2954@gmail.com>
2026-02-03 18:56:54 +0530

    router link issue solved in resister page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Register } from './Component/UnAuth/register/register';
 import { Login } from './Component/UnAuth/login/login';
 
 export const routes: Routes = [
+    { path: '', component:Login},
     { path: 'login', component:Login},
     { path: 'register',component:Register}
 ];
