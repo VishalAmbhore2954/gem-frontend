2d94a1405d54a42d4d7ea8f7de189889362b1264
Vishal Ambhore <vishalambhore2954@gmail.com>
2026-02-07 21:11:40 +0530

    made header and stock mobile responsive

--- src/app/Component/Auth/header/header.ts
+++ src/app/Component/Auth/header/header.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Import this
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  standalone: true, // Ensure this is present
+  imports: [RouterLink, CommonModule], // Add CommonModule here
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
