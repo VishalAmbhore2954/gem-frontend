a288233bf406d908b894b47e7b79d619067cf9e6
Vishal Ambhore <vishalambhore2954@gmail.com>
2026-02-07 21:18:33 +0530

    home mobile responsive

--- src/app/Component/Auth/home/home.ts
+++ src/app/Component/Auth/home/home.ts
@@ -1,14 +1,14 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-home',
-  imports: [CommonModule,RouterLink],
-  templateUrl: './home.html',
-  styleUrl: './home.css',
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterLinkActive],
+  templateUrl: './dashboard.html',
 })
-export class Home {
+export class Dashboard {
   sidebarOpen = false;
 
   toggleSidebar() {
